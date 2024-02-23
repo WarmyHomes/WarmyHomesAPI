b821ebc1cc1e37698cd3eaf9bebc84b4b949de47
Emre Akkaş <132943633+Akkasemre@users.noreply.github.com>
2024-02-23 21:16:37 +0300

    proje dosyaları eklendi

---
+++ Warmy-Homes/src/main/java/com/project/WarmyHomesApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmyHomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmyHomesApplication.class, args);
+	}
+
+}
