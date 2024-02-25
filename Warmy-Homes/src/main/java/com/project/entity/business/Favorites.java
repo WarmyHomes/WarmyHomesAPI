6d1ed1cd01586439bfaebe19aad0312507302db7
Emre Akkaş <akkasemre.0021@gmail.com>
2024-02-25 14:49:30 +0300

    contacts-favorites entity yazıldı

--- Warmy-Homes/src/main/java/com/project/entity/business/Favorites.java
+++ Warmy-Homes/src/main/java/com/project/entity/business/Favorites.java
@@ -1,4 +1,32 @@
 package com.project.entity.business;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder(toBuilder = true)
 public class Favorites {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //oneToMany
+    private long userId;
+
+    //oneToMany
+    private long advertId;
+
+    private Date createAt;
+
+
 }
