192e7333324ae9d6227a8b47cd291480e7490463
Emre AKTAS <aktasemre1988@mail.com>
2024-02-26 02:05:52 +0300

    image ve districs tamamlandi imageType icin enum dosyasi olusturuldu

--- Warmy-Homes/src/main/java/com/project/entity/business/Districts.java
+++ Warmy-Homes/src/main/java/com/project/entity/business/Districts.java
@@ -1,5 +1,33 @@
 package com.project.entity.business;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder(toBuilder = true)
 public class Districts {
     //EmreAktas
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+
+    //TODO class ismini duzelt
+    @ManyToOne
+    @JoinColumn(name = "city_id", nullable = false)
+    private Cities city;
+
+
 }
+
+
--- Warmy-Homes/src/main/java/com/project/entity/business/Images.java
+++ Warmy-Homes/src/main/java/com/project/entity/business/Images.java
@@ -1,5 +1,49 @@
 package com.project.entity.business;
 
+import com.project.entity.enums.ImageType;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder(toBuilder = true)
+//TODO class ismini duzelt
 public class Images {
     //EmreAktas
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob //büyük boyutlu datalar için sütun oluşmasını sağlar
+    @Column(nullable = false)
+    private byte[] data;
+
+    @Column(nullable = false)
+    private String name;
+
+
+    //@Column
+    //private String type;
+    //todo emun tipinde olsa daha mi iyi olur?
+    @Enumerated(EnumType.STRING) // Enum olarak saklanacak
+    @Column(nullable = false)
+    private ImageType type;
+
+    @Column(nullable = false)
+    private boolean featured;
+
+    @ManyToOne
+    @JoinColumn(name = "advert_id", nullable = false)
+    private Adverts advert;
+
+
 }
+
+
+
+
