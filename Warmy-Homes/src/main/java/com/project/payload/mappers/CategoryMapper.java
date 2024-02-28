622fc734d88c7a5aff25d01d48fc528b48682b03
Emre AKTAS <aktasemre1988@mail.com>
2024-02-28 22:19:04 +0300

    category  dto=>entity ve entit=>dto donusumleri yapildi

--- Warmy-Homes/src/main/java/com/project/payload/mappers/CategoryMapper.java
+++ Warmy-Homes/src/main/java/com/project/payload/mappers/CategoryMapper.java
@@ -2,30 +2,36 @@ package com.project.payload.mappers;
 
 import com.project.entity.business.Category;
 import com.project.payload.request.business.CategoryDTO;
-import org.springframework.stereotype.Component;
 
-@Component
+import java.time.LocalDateTime;
+
 public class CategoryMapper {
 
-
-
-
-
-
-
-    private CategoryDTO mapCategoryToCategoryRequest(Category category) {
-        return null;
-
-
-
-
-          //Category.builder()
-          //
-
-
-
-
-          //.build();
+    public static Category mapCategoryDTOToEntity(CategoryDTO categoryDTO) {
+        return Category.builder()
+                .id(categoryDTO.getId())
+                .title(categoryDTO.getTitle())
+                .icon(categoryDTO.getIcon())
+                .seq(categoryDTO.getSeq())
+                .slug(categoryDTO.getSlug())
+               //todo buraya bak
+                // .isActive(categoryDTO.isActive())
+               // .createAt(LocalDateTime.parse(categoryDTO.getCreateAt()))
+               // .updateAt(categoryDTO.getUpdateAt() != null ? LocalDateTime.parse(categoryDTO.getUpdateAt()) : null)
+                .build();
     }
 
+    public static CategoryResponse mapCategoryToResponse(Category category) {
+        return CategoryResponse.builder()
+                .id(category.getId())
+                .title(category.getTitle())
+                .icon(category.getIcon())
+                .builtIn(category.isBuiltIn())
+                .seq(category.getSeq())
+                .slug(category.getSlug())
+                .isActive(category.isActive())
+                .createAt(category.getCreateAt().toString())
+                .updateAt(category.getUpdateAt() != null ? category.getUpdateAt().toString() : null)
+                .build();
+    }
 }
