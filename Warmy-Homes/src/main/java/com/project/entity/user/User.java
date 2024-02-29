f7a705eb249ad6de7edb2e65a26dcd4773d734d8
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-02-29 21:45:34 +0300

    [Update]: Validate
    Validation has been added to the fields in the entity.

--- Warmy-Homes/src/main/java/com/project/entity/user/User.java
+++ Warmy-Homes/src/main/java/com/project/entity/user/User.java
@@ -8,8 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -24,27 +28,40 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "First name must not be empty! ")
+    @Size(min=2 , max =30 , message= "First name should be at least 2 characters!")
     private String first_name;
 
+    @NotNull(message = "Last name must not be empty! ")
+    @Size(min=2 , max =30 , message= "Last name should be at least 2 characters!")
     private String last_name;
 
+    @NotNull(message = "Email must not be empty!")
+    @Email(message = "Please, enter valid email!")
+    @Size(min=10, max=80 , message = "Your email should be between 10 and 80 chars!")
     @Column(unique = true)
     private String email;
 
+    @NotNull(message = "Phone number must not be empty!")
     private String phone;
 
+    @NotNull(message = "Phone number must not be empty!")
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)//Sadece DB'e kayit yaptik ve Response'da password DTO class'da olmasin diye yaptim
     private String password_hash;
 
+    @Nullable
     private String reset_password_code;
 
     private Boolean built_in;
 
+    @NotNull(message = "Create date must not be empty!")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "US")
     private LocalDateTime create_at;
 
+    @Nullable
     private LocalDateTime update_at;
 
+    //----- Relations ------
     @OneToOne
     //@JsonProperty(access = JsonProperty.Access.READ_WRITE)//Sadece DB'ye kayit yaptik Response'da Role donmeyecek
     private UserRole userRole;
