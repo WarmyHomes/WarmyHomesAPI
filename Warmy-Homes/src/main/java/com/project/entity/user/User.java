094ba7cf1d761b1ad3bfd66e431ed16765a17f41
Nejla KÜÇÜK <nkucuk097@gmail.com>
2024-02-29 21:51:10 +0300

    [Update]: Import builder annotation

--- Warmy-Homes/src/main/java/com/project/entity/user/User.java
+++ Warmy-Homes/src/main/java/com/project/entity/user/User.java
@@ -12,10 +12,7 @@ import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.lang.Nullable;
 
 @Getter
