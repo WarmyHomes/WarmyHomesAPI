f5541408408ebe62465e641ec0145a3f6a8954f4
hocabilgic <hocabilgic@gmail.com>
2024-03-05 19:08:41 +0300

    SettingService

--- Warmy-Homes/src/main/java/com/project/service/business/SettingsService.java
+++ Warmy-Homes/src/main/java/com/project/service/business/SettingsService.java
@@ -3,23 +3,39 @@ package com.project.service.business;
 import com.project.entity.business.Advert;
 import com.project.entity.business.Category;
 import com.project.entity.business.helperentity.Advert_Type;
-import com.project.repository.business.AdvertRepository;
-import com.project.repository.business.AdvertTypesRepository;
-import com.project.repository.business.CategoryRepository;
-import com.project.repository.business.ImageRepository;
+import com.project.entity.user.User;
+import com.project.entity.user.UserRole;
+import com.project.repository.business.*;
+
+import com.project.repository.helperRepository.CategoryPropertyKeyRepository;
+import com.project.repository.helperRepository.CategoryPropertyValueRepository;
+import com.project.repository.user.UserRepository;
+import com.project.repository.user.UserRoleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.management.relation.Role;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SettingsService {
+
     private final AdvertRepository advertRepository;
     private final ImageRepository imageRepository;
     private final CategoryRepository categoryRepository;
     private final AdvertTypesRepository advertTypesRepository;
-    private
+    private final CategoryPropertyKeyRepository categoryPropertyKeyRepository;
+    private final CategoryPropertyValueRepository categoryPropertyValueRepository;
+    private final FavoritesRepository favoritesRepository;
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final AddressCountryRepository addressCountryRepository;
+    private final AddressCityRepository addressCityRepository;
+    private final AddressDistrictRepository addressDistrictRepository;
+    private final TourRequestRepository tourRequestRepository;
+
+
 
 
     public void resetDatabase() {
@@ -51,7 +67,7 @@ public class SettingsService {
 
         List<Advert_Type> allAdvertType=advertTypesRepository.findAll();
         for (Advert_Type advertType:allAdvertType){
-            if (!advertType.getBuiltin;){
+            if (!advertType.getBuiltIn()){
                 advertTypesRepository.deleteAll();
             }
         }
